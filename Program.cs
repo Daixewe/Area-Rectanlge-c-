76e2ed55afdaac2f5457030ae23b5807a6fce892
Daixewe <fuentesvd56@gmail.com>
2022-06-15 13:48:04 -0600

    Area of a rectangle in c#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Area of a rectangle");
+ int Base, Height;
+ 
+ int Area;
+
+Console.WriteLine("insert the base of the rectangle");
+Base = int.Parse(Console.ReadLine());
+Console.WriteLine("Insert the height if the rectangle");
+Height = int.Parse(Console.ReadLine());
+
+Area = Base * Height;
+Console.WriteLine("The area of the rectangle is");
+Console.WriteLine(Area);
+
